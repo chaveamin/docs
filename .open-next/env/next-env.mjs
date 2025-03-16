9943573df69688438b599bd1a812b9dc9d059605
chaveamin <aminchave@gmail.com>
2025-03-16 12:30:27 +0330

    cf config

---
+++ .open-next/env/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_PASSWORD_LAGOM":"4LPqnfeXDuYUhKNHj7"};
+export const development = {"NEXT_PUBLIC_PASSWORD_LAGOM":"4LPqnfeXDuYUhKNHj7"};
+export const test = {};
