e3833deee76adde405015a714a793d08425e9c25
chaveamin <aminchave@gmail.com>
2025-07-16 07:41:12 +0330

    client

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,12 +30,12 @@ function TrafficLightsIcon(props) {
 
 export function Hero() {
   return (
-    <div className="overflow-hidden bg-slate-900 dark:-mb-32 dark:mt-[-4.5rem] dark:pb-32 dark:pt-[4.5rem] dark:lg:mt-[-4.75rem] dark:lg:pt-[4.75rem]">
+    <div className="overflow-hidden bg-slate-900 dark:mt-[-4.5rem] dark:-mb-32 dark:pt-[4.5rem] dark:pb-32 dark:lg:mt-[-4.75rem] dark:lg:pt-[4.75rem]">
       <div className="py-16 sm:px-2 lg:relative lg:px-0 lg:py-20">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 lg:max-w-8xl lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
+        <div className="lg:max-w-8xl mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
           <div className="relative z-10 md:text-center lg:text-left">
             <Image
-              className="absolute bottom-full right-full -mb-56 -mr-72 opacity-50"
+              className="absolute right-full bottom-full -mr-72 -mb-56 opacity-50"
               src={blurCyanImage}
               alt="hero-image"
               width={530}
@@ -44,11 +44,8 @@ export function Hero() {
               priority
             />
             <div className="relative text-right">
-              <p className="inline bg-linear-to-l from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text text-6xl  font-bold  text-transparent">
+              <p className="inline bg-linear-to-l from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text text-6xl font-bold text-transparent">
                 راهنمای محصولات
-              </p>
-              <p className="mt-3 text-2xl  text-slate-400">
-                راهنمای محصولات امین چاوه پور
               </p>
               <div className="mt-8 flex gap-4 md:justify-center lg:justify-start">
                 <Button target="_blank" href="https://designesia.ir/">
@@ -65,8 +62,8 @@ export function Hero() {
             </div>
           </div>
           <div className="relative lg:static xl:pl-10">
-            <div className="absolute inset-x-[-50vw] -bottom-48 -top-32 [mask-image:linear-gradient(transparent,white,white)] lg:-bottom-32 lg:-top-32 lg:left-0 lg:right-[calc(50%+14rem)] lg:[mask-image:none] dark:[mask-image:linear-gradient(transparent,white,transparent)] lg:dark:[mask-image:linear-gradient(white,white,transparent)]">
-              <HeroBackground className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 lg:left-0 lg:translate-x-0 lg:translate-y-[-60%]" />
+            <div className="absolute inset-x-[-50vw] -top-32 -bottom-48 [mask-image:linear-gradient(transparent,white,white)] lg:-top-32 lg:right-[calc(50%+14rem)] lg:-bottom-32 lg:left-0 lg:[mask-image:none] dark:[mask-image:linear-gradient(transparent,white,transparent)] lg:dark:[mask-image:linear-gradient(white,white,transparent)]">
+              <HeroBackground className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 lg:left-0 lg:translate-x-0 lg:translate-y-[-60%]" />
             </div>
           </div>
         </div>
