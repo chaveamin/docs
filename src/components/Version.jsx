a3739a9facdcb5b32c0a59af9d3929e8d71db885
chaveamin <aminchave@gmail.com>
2025-07-21 08:31:48 +0330

    mail

--- src/components/Version.jsx
+++ src/components/Version.jsx
@@ -1,6 +1,6 @@
 export default function Version({ v }) {
   return (
-    <span className="rounded-sm bg-blue-100 px-1.5 py-0.5 text-sm font-light text-blue-600">
+    <span className="ver rounded-sm font-mono bg-blue-100 px-1.5 py-1 text-sm font-light text-blue-600">
       {v}
     </span>
   )
